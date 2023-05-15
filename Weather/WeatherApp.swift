650b7203ea1e6b137df5520ee19f76d01876fe1a
Maedeh Daemi <maede.daemi@gmail.com>
2023-05-15 08:52:40 +0200

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Maedeh DAEMI on 15/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
