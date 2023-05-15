4d74e9c1bfcf7782355680037fe6d44c41a79dc0
Maedeh Daemi <maede.daemi@gmail.com>
2023-05-15 11:07:43 +0200

    Add weatherViewPage

--- Weather/Presentation/View/WeatherPageView.swift
+++ Weather/Presentation/View/WeatherPageView.swift
@@ -8,8 +8,65 @@
 import SwiftUI
 
 struct WeatherPageView: View {
+    @State var value : Float = 0
+    @State private var loadingMessage: String = localized("weatherPage.message1")
+    @State private var restart: Bool = true
+    let loadingTime: Float = 60
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.green, .white]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 32) {
+                if restart {
+                    Text(loadingMessage)
+                        .foregroundColor(.primary)
+                        .font(.system(size: 16, weight: .bold))
+                        .frame(alignment: .leading)
+                        .frame(maxWidth: .infinity)
+                    ProgressView(value: value, total: loadingTime)
+                        .progressViewStyle(LinearProgressViewStyle())
+                        .padding(.horizontal, 32)
+                        .frame(height: 32)
+                        .onAppear {
+                            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+                                value += 1
+                                
+                                if Int(value).isMultiple(of: 6) {
+                                    loadingMessage = getNextMessage(of: loadingMessage)
+                                }
+                                
+                                if value == loadingTime {
+                                    timer.invalidate()
+                                    restart = false
+                                    value = 0
+                                }
+                            }
+                        }
+                } else {
+                    Button("Start") {
+                        restart = true
+                    }
+                }
+            }
+        }
+    }
+    
+    private func getNextMessage(of currentMessage: String) -> String {
+        var nextMessage = ""
+        let messages: [String] = [localized("weatherPage.message1"),
+                                  localized("weatherPage.message2"),
+                                  localized("weatherPage.message3")]
+        messages.enumerated().forEach { (key, value) in
+            if value == currentMessage {
+                if key == 2 {
+                    nextMessage = messages.first!
+                } else {
+                    nextMessage = messages[key+1]
+                }
+            }
+        }
+        return nextMessage
     }
 }
 
