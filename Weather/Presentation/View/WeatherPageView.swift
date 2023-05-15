6a03289f7aa68d71b6a40aec590cde43fde7e011
Maedeh Daemi <maede.daemi@gmail.com>
2023-05-15 11:24:01 +0200

    Add progressView to the weather page

--- Weather/Presentation/View/WeatherPageView.swift
+++ Weather/Presentation/View/WeatherPageView.swift
@@ -8,47 +8,59 @@
 import SwiftUI
 
 struct WeatherPageView: View {
-    @State var value : Float = 0
+    @State private var value : Float = 0
     @State private var loadingMessage: String = localized("weatherPage.message1")
     @State private var restart: Bool = true
-    let loadingTime: Float = 60
+    let loadingTime: Float = 20
+
     
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.green, .white]), startPoint: .top, endPoint: .bottom)
                 .edgesIgnoringSafeArea(.all)
-            VStack(spacing: 32) {
+            VStack(spacing: 48) {
                 if restart {
                     Text(loadingMessage)
                         .foregroundColor(.primary)
-                        .font(.system(size: 16, weight: .bold))
-                        .frame(alignment: .leading)
+                        .font(.system(size: 18, weight: .bold))
+                        .frame(alignment: .center)
                         .frame(maxWidth: .infinity)
-                    ProgressView(value: value, total: loadingTime)
-                        .progressViewStyle(LinearProgressViewStyle())
-                        .padding(.horizontal, 32)
-                        .frame(height: 32)
-                        .onAppear {
-                            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
-                                value += 1
-                                
-                                if Int(value).isMultiple(of: 6) {
-                                    loadingMessage = getNextMessage(of: loadingMessage)
-                                }
-                                
-                                if value == loadingTime {
-                                    timer.invalidate()
-                                    restart = false
-                                    value = 0
+                        .lineSpacing(8)
+                        .padding(8)
+
+                    ZStack {
+                        ProgressView(value: value, total: loadingTime)
+                            .progressViewStyle(GaugeProgressStyle())
+                            .frame(width: 150, height: 150)
+                            .contentShape(Rectangle())
+                            .overlay(
+                                Text("\(Int(value)) %")
+                                    .foregroundColor(.primary)
+                                    .font(.system(size: 24, weight: .bold))
+                            )
+                            .onAppear {
+                                Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+                                    value += 1
+                                    
+                                    if Int(value).isMultiple(of: 6) {
+                                        loadingMessage = getNextMessage(of: loadingMessage)
+                                    }
+                                    
+                                    if value == loadingTime {
+                                        timer.invalidate()
+                                        restart = false
+                                        value = 0
+                                    }
                                 }
                             }
-                        }
+                    }
+                    
                 } else {
                     Button("Start") {
                         restart = true
                     }
                 }
-            }
+            }.padding()
         }
     }
     
@@ -67,6 +79,22 @@ struct WeatherPageView: View {
             }
         }
         return nextMessage
+    }
+}
+
+struct GaugeProgressStyle: ProgressViewStyle {
+    var strokeColor = Color.blue
+    var strokeWidth = 25.0
+
+    func makeBody(configuration: Configuration) -> some View {
+        let fractionCompleted = configuration.fractionCompleted ?? 0
+
+        return ZStack {
+            Circle()
+                .trim(from: 0, to: fractionCompleted)
+                .stroke(strokeColor, style: StrokeStyle(lineWidth: strokeWidth, lineCap: .round))
+                .rotationEffect(.degrees(-90))
+        }
     }
 }
 
